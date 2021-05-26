d63440ade52d90a476c416fc61ed1a1564d8a115
omarijalagania <omari.jalagania@gmail.com>
2021-05-26 09:00:59 +0400

    layers

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import MealsNavigator from './navigation/MealsNavigator';
+
 const fetchFonts = () => {
 	Font.loadAsync({
 		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
